eb70946b824cbbe740814d8ea4ad1dee51aaad9d
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-04-21 20:15:03 -0300

    refatorando usando axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,47 @@
-export default function Home(props) {
-  console.log(props.episodes)
+import { GetStaticProps } from 'next'
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { api } from '../services/api'
 
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+}
+type HomeProps = {
+  episodes: Episode[]
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <h1>INDEX</h1>
+    <>
+      <h1>INDEX</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </>
   )
 }
 
-export async function getStaticProps() {
-  const res = await fetch('http://localhost:3333/episodes')
-  const data = await res.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: { 
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const episodes = data.map(episode => {
+    return{
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+      duration: Number(episode.file.duration),
+      description: episode.description,
+      url: episodes.file.url
+    }
+  })
 
   return {
     props: { episodes: data },
