b004f7ffb58a5d8180d13d8830b59eed4bf8ca21
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-04-20 21:22:56 -0300

    consumindo dados com SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
-import { useEffect } from "react"
-
-export default function Home() {
-  useEffect(() => {
-    fetch('http://localhost:3333/episodes')
-      .then(res => res.json())
-      .then(data => console.log(data))
-  }, [])
+export default function Home(props) {
+  console.log(props.episodes)
 
   return (
     <h1>INDEX</h1>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch('http://localhost:3333/episodes')
+  const data = await res.json()
+
+  return {
+    props: { episodes: data }
+  }
 }
