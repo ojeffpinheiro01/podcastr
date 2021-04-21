442221b47282b5f90f76a5f029fb74f54b150936
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-04-20 21:17:33 -0300

    consumindo dados com SPA

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react"
+
 export default function Home() {
+  useEffect(() => {
+    fetch('http://localhost:3333/episodes')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [])
+
   return (
     <h1>INDEX</h1>
   )
