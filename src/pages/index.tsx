e4cc89a609e9cede00bf310af2bfaa96e0ff5494
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-04-21 20:42:53 -0300

    tipagem dos episodios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,18 @@ import { GetStaticProps } from 'next'
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 import { api } from '../services/api'
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString'
 
 type Episode = {
   id: string;
   title: string;
+  thumbnail: string;
+  description: string;
   members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  published_at: string;
 }
 type HomeProps = {
   episodes: Episode[]
@@ -38,13 +45,14 @@ export const getStaticProps: GetStaticProps = async () => {
       members: episode.members,
       publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
       duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
       description: episode.description,
-      url: episodes.file.url
+      url: episode.file.url
     }
   })
 
   return {
-    props: { episodes: data },
+    props: { episodes },
     revalidate: 60 * 60 * 8
   }
 }
