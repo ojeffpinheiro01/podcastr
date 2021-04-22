61c46aded73f089b603fb9afe92e01a3299ce141
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-04-21 22:24:52 -0300

    redirecionando pra página home

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,6 +1,7 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
+import Link from 'next/link'
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 
@@ -30,9 +31,12 @@ export default function Episode({ episode }: EpisodeProps) {
     return (
         <div className={styles.episode} >
             <div className={styles.thumbnailContainer}>
-                <button type='button'>
+                <Link href={'/'}>
+                    <button type='button'>
                     <img src='/arrow-left.svg' alt='Voltar' />
                 </button>
+                </Link>
+                
 
                 <Image
                     width={700}
