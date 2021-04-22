7ef3398285ae61682965862787cb1298a858a900
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-04-21 21:33:02 -0300

    página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
           {latestEpisodes.map(episode => {
             return (
               <li key={episode.id}>
-                <Image 
+                <Image
                   width={120}
                   height={120}
                   src={episode.thumbnail}
@@ -57,6 +57,42 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
       </section>
 
       <section className={styles.allEpisodes}>
+        <h2>Todos episódios</h2>
+        <table cellSpacing={0}>
+          <thead>
+            <th></th>
+            <th>Podcast</th>
+            <th>Integrantes</th>
+            <th>Data</th>
+            <th>Duração</th>
+            <th></th>
+          </thead>
+          <tbody>
+            {allEpisodes.map(episode => {
+              return (
+                <tr key={episode.id}>
+                  <td>
+                    <Image
+                      width={120}
+                      height={120}
+                      src={episode.thumbnail}
+                      alt={episode.title}
+                      objectFit="cover" />
+                  </td>
+                  <td><a href=''>{episode.title}</a></td>
+                  <td>{episode.members}</td>
+                  <td style={{ width: 100 }} >{episode.publishedAt}</td>
+                  <td>{episode.durationAsString}</td>
+                  <td>
+                    <button type="button">
+                      <img src="/play-green.svg" alt='play' />
+                    </button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </section>
     </div>
   )
@@ -64,7 +100,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
 export const getStaticProps: GetStaticProps = async () => {
   const { data } = await api.get('episodes', {
-    params: { 
+    params: {
       _limit: 12,
       _sort: 'published_at',
       _order: 'desc'
@@ -72,12 +108,12 @@ export const getStaticProps: GetStaticProps = async () => {
   })
 
   const episodes = data.map(episode => {
-    return{
+    return {
       id: episode.id,
       title: episode.title,
       thumbnail: episode.thumbnail,
       members: episode.members,
-      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
       duration: Number(episode.file.duration),
       durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
       description: episode.description,
